ddb32eb1bb9fbe61a6ccd4446573eec1cac38d08
sahilRs <97721777+sahilRs@users.noreply.github.com>
2025-06-11 10:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/my.json", methods=["GET"])
 def get_data():
     data = [
@@ -18,9 +17,20 @@ def get_data():
             "expiresAt": "2025-06-15",
             "allowOffline": False,
         },
+        {
+            "id": "370aadebb16b8920",
+            "key": "MITRA-CUSTOM-1122",
+            "expiresAt": "2026-06-20",
+            "allowOffline": True,
+        },
+        {
+            "id": "f0a09d2316ae125a3",
+            "key": "MITRA-CUSTOM-3344",
+            "expiresAt": "2026-06-20",
+            "allowOffline": True,
+        },
     ]
     return jsonify(data)
-
 
 if __name__ == "__main__":
     app.run()
